a614783f926be21fe6b7dab464be40d85ecb5a52
김준우 <kjunwoo23@naver.com>
2024-09-19 22:11:47 +0900

    [Platinum V] Title: 개미, Time: 64 ms, Memory: 10244 KB -BaekjoonHub

--- 백준/Platinum/14942. 개미/개미.cc
+++ 백준/Platinum/14942. 개미/개미.cc
@@ -25,22 +25,26 @@ bool visited[100001];
 
 tuple<int, int> memo[100001];
 
+// 1번 방에서 출발, 모든 방 탐색
 void DFS(int node) {
 	visited[node] = true;
 
 	for (int i = 0; i < edges[node].size(); i++)
 		if (visited[get<0>(edges[node][i])]) continue;
 		else {
+			// 새로운 방 찾을 때마다 (새로운 방, 방까지의 거리) 쌍 저장
 			antPath[get<0>(edges[node][i])] = tuple<int, int>(node, get<1>(edges[node][i]));
 			DFS(get<0>(edges[node][i]));
 		}
 }
 
+// i번째 개미의 모험
 int MoveAnt(int i) {
 	int curRoom = i;
 	int losedPower = 0;
 	while (antPower[i] - get<1>(antPath[curRoom]) >= 0) {
 		if (curRoom == 1) break;
+		//전에 죽은 개미가 남긴 메모가 있는지 확인
 		if (get<0>(memo[i]) != 0)
 			if (get<1>(memo[i]) <= antPower[i]) {
 				antPower[i] -= get<1>(memo[i]);
@@ -48,10 +52,12 @@ int MoveAnt(int i) {
 				curRoom = get<0>(memo[i]);
 				continue;
 			}
+		// 다음 방으로 이동할 때는 DFS에서 찾아둔 경로대로 이동
 		antPower[i] -= get<1>(antPath[curRoom]);
 		losedPower += get<1>(antPath[curRoom]);
 		curRoom = get<0>(antPath[curRoom]);
 	}
+	// 개미가 죽을 때마다 메모를 남김 (출발한 방, 도착한 방, 총 소요 힘)
 	memo[i] = tuple<int, int>(curRoom, losedPower);
 	return curRoom;
 }
@@ -76,6 +82,7 @@ int main() {
 	DFS(1);
 
 	for (int i = 1; i < N + 1; i++)
+		//"\n"!!!!!!!!!!!!!!!
 		cout << MoveAnt(i) << "\n";
 
 	return 0;
