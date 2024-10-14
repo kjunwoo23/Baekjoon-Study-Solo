43e2e8b36b87c5c672e68e72268569bffe068f2f
김준우 <kjunwoo23@naver.com>
2024-10-14 19:39:44 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,13 +1,13 @@
-#include <iostream>
-#include <algorithm>
-#include <queue>
-#include <climits>
+#include <bits/stdc++.h>
 using namespace std;
 
 char route[101][101] = { 0 };
 int dist[101][101] = { 0 };
 bool visited[101][101] = { 0 };
 int N, M;
+
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, -1, 0, 1 };
 
 void inputRoute() {
     for (int i = 0; i < N; i++) {
@@ -20,7 +20,7 @@ void inputRoute() {
 void getDistance() {
     queue<pair<int, int>> container;
 
-    container.push(make_pair(0, 0));
+    container.emplace(0, 0);
 
     while (!container.empty()) {
         pair<int, int> nowPos = container.front();
@@ -34,27 +34,15 @@ void getDistance() {
         if (visited[i][j]) continue;
         visited[i][j] = true;
 
-        if (i > 0)
-            if (!visited[i - 1][j] && route[i - 1][j] == '1') {
-                container.push(make_pair(i - 1, j));
-                dist[i - 1][j] = min(dist[i - 1][j], dist[i][j] + 1);
-            }
-        if (i < N - 1)
-            if (!visited[i + 1][j] && route[i + 1][j] == '1') {
-                container.push(make_pair(i + 1, j));
-                dist[i + 1][j] = min(dist[i + 1][j], dist[i][j] + 1);
-            }
-        if (j > 0)
-            if (!visited[i][j - 1] && route[i][j - 1] == '1') {
-                container.push(make_pair(i, j - 1));
-                dist[i][j - 1] = min(dist[i][j - 1], dist[i][j] + 1);
-            }
-        if (j < M - 1)
-            if (!visited[i][j + 1] && route[i][j + 1] == '1') {
-                container.push(make_pair(i, j + 1));
-                dist[i][j + 1] = min(dist[i][j + 1], dist[i][j] + 1);
-            }
-        
+        for (int d = 0; d < 4; d++) {
+            if (0 <= i + dy[d] && i + dy[d] < N)
+                if (0 <= j + dx[d] && j + dx[d] <M) {
+                    if (!visited[i + dy[d]][j + dx[d]] && route[i + dy[d]][j + dx[d]] == '1') {
+                        container.emplace(i + dy[d], j + dx[d]);
+                        dist[i + dy[d]][j + dx[d]] = min(dist[i + dy[d]][j + dx[d]], dist[i][j] + 1);
+                    }
+                }
+        }
     }
 }
 
