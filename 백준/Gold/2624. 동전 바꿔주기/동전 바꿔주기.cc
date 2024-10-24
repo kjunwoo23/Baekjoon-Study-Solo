4718df40866ec008904fe2b91cbf11b1b86ff8b4
김준우 <kjunwoo23@naver.com>
2024-10-24 22:11:28 +0900

    [Gold IV] Title: 동전 바꿔주기, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.cc
+++ 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.cc
@@ -9,6 +9,8 @@ long long memo[10002];
 void ChangeCoin() {
     memo[0] = 1;
 
+    // 동전 종류마다 경우의 수 + 1
+    // 뒤에서 앞으로 오면서 동전 개수만큼 경우의 수 더하기
     for (int i = 0; i < k; i++) {
         for (int j = t; j >= 0; j--)
             for (int coinNum = 1; coinNum <= coins[i].second; coinNum++) {
@@ -30,7 +32,7 @@ int main() {
         cin >> tmp1 >> tmp2;
         coins.emplace_back(tmp1, tmp2);
     }
-    sort(coins.begin(), coins.end());
+    //sort(coins.begin(), coins.end());
 
     ChangeCoin();
 
