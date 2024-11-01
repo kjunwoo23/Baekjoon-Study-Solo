d55ad98341174ca1e472dd2051efe45c0153fe45
김준우 <kjunwoo23@naver.com>
2024-11-01 23:48:10 +0900

    [Gold V] Title: 최소비용 구하기, Time: 40 ms, Memory: 7064 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M, start, fin;
+vector<pair<int, int>> edges[100001];
+
+int dist[1001];
+
+void BFS() {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.emplace(0, start);
+    dist[start] = 0;
+
+    while (!pq.empty()) {
+        int curPrice = pq.top().first;
+        int curNode = pq.top().second;
+        pq.pop();
+
+        if (curPrice > dist[curNode]) continue;
+        if (dist[curNode] == INT_MAX / 2) continue;
+
+        for (int i = 0; i < edges[curNode].size(); i++) {
+            if (dist[edges[curNode][i].second] > dist[curNode] + edges[curNode][i].first) {
+                dist[edges[curNode][i].second] = dist[curNode] + edges[curNode][i].first;
+                pq.emplace(dist[edges[curNode][i].second], edges[curNode][i].second);
+            }            
+        }
+    }
+}
+
+
+int main()
+{
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> N >> M;
+
+    fill(&dist[0], &dist[N] + 1, INT_MAX / 2);
+
+    int tmp1, tmp2, tmp3;
+    for (int i = 0; i < M; i++) {
+        cin >> tmp1 >> tmp2 >> tmp3;
+        edges[tmp1].emplace_back(tmp3, tmp2);
+    }
+
+    cin >> start >> fin;
+
+    BFS();
+
+    cout << dist[fin];
+
+    return 0;
+}
