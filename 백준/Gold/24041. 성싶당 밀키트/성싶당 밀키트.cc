7d8e680875a4df679278412ce66b34a2dbbcd315
김준우 <kjunwoo23@naver.com>
2024-11-16 05:33:18 +0900

    [Gold IV] Title: 성싶당 밀키트, Time: 380 ms, Memory: 8296 KB -BaekjoonHub

--- 백준/Gold/24041. 성싶당 밀키트/성싶당 밀키트.cc
+++ 백준/Gold/24041. 성싶당 밀키트/성싶당 밀키트.cc
@@ -20,18 +20,22 @@ void init() {
 
 bool cntGerm(int day) {
     ll totalGerm = 0;
-
+    
+    //필수 재료들 세균값 더하기
     for (int i = 0; i < important.size(); i++) {
         totalGerm += (ll)important[i].S * max(1, day - important[i].L);
     }
-
+    
+    //선택 재료들 세균값 우선순위 큐에 저장 (내림차순)
     priority_queue<ll> notImportantPQ;
-
+    
+    //선택 재료들 일단 모두 다 결과값에 더하기
     for (int i = 0; i < notImportant.size(); i++) {
         totalGerm += (ll)notImportant[i].S * max(1, day - notImportant[i].L);
         notImportantPQ.emplace((ll)notImportant[i].S * max(1, day - notImportant[i].L));
     }
-
+    
+    //가장 작은 K개 결과값에서 빼기
     for (int i = 0; i < K; i++) {
         if (notImportantPQ.empty()) break;
         totalGerm -= notImportantPQ.top();
