e5726afece76aa1301446c47ff71501ca2e42792
김준우 <kjunwoo23@naver.com>
2024-12-02 03:01:31 +0900

    [Gold II] Title: 케이크 자르기 2, Time: 8 ms, Memory: 9856 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Gold/10714. 케이크 자르기 2/케이크 자르기 2.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+typedef long long ll;
+
+using namespace std;
+
+void init() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+}
+
+int N;
+ll ans;
+ll dp[1001][1001];
+ll cake[1001];
+
+int clamp(int i) {
+    if (i < 0) return i + N;
+    if (i >= N) return i - N;
+    return i;
+}
+
+ll EatCake(int start, int end) {
+    if (dp[start][end] != 0) return dp[start][end];
+    if (clamp(start) == clamp(end + 1) || clamp(start) == clamp(end + 2)) return 0;
+
+    if (cake[clamp(start - 1)] > cake[clamp(end + 1)])
+        start = clamp(start - 1);
+    else
+        end = clamp(end + 1);
+
+    ll tmp1 = cake[clamp(start - 1)] + EatCake(clamp(start - 1), end);
+    ll tmp2 = cake[clamp(end + 1)] + EatCake(start, clamp(end + 1));
+
+    dp[start][end] = max(tmp1, tmp2);
+
+    return dp[start][end];
+}
+
+int main() {
+    init();
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> cake[i];
+        dp[i][i] = cake[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        memset(dp, 0, sizeof(dp));
+        ans = max(ans, cake[i] + EatCake(i, i));
+    }
+
+    cout << ans;
+    return 0;
+}
