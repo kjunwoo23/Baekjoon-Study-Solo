2d77325cd4f93cf4096538b43fcbfd6e4cd577f2
김준우 <kjunwoo23@naver.com>
2024-12-02 23:10:41 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,62 +1,69 @@
 #include <bits/stdc++.h>
-#include <unordered_map>
+#define endl '\n'
+typedef long long ll;
 
 using namespace std;
 
-int N, M, V;
-
-vector<int> adjNodes[1001];
-bool visited[1001];
-
-void DFS(int node) {
-    if (visited[node]) return;
-    
-    visited[node] = true;
-    cout << node << ' ';
-
-    for (int i = 0; i < adjNodes[node].size(); i++)
-        DFS(adjNodes[node][i]);
+void init() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
 }
 
-void BFS(int start) {
+int N, M, V;
+vector<int> edges[1001];
+bool visited[1001];
+
+void DFS(int v) {
+    if (visited[v]) return;
+    visited[v] = true;
+
+    cout << v << ' ';
+
+    for (int i = 0; i < edges[v].size(); i++) {
+        DFS(edges[v][i]);
+    }
+}
+
+void BFS(int v) {
     queue<int> waitList;
 
-    waitList.emplace(start);
-    visited[start] = true;
+    waitList.emplace(v);
+    visited[v] = true;
 
     while (!waitList.empty()) {
-        int curNode = waitList.front();
+        int tmpV = waitList.front();
         waitList.pop();
-        cout << curNode << ' ';
 
-        for (int i = 0; i < adjNodes[curNode].size(); i++) {
-            if (visited[adjNodes[curNode][i]]) continue;
-            visited[adjNodes[curNode][i]] = true;
-            waitList.emplace(adjNodes[curNode][i]);
+        cout << tmpV << ' ';
+
+        for (int i = 0; i < edges[tmpV].size(); i++) {
+            if (!visited[edges[tmpV][i]]) {
+                visited[edges[tmpV][i]] = true;
+                waitList.emplace(edges[tmpV][i]);
+            }
         }
     }
 }
 
-int main()
-{
-    cin.tie(NULL); cout.tie(NULL);
-    ios_base::sync_with_stdio(0);
+int main() {
+    init();
 
     cin >> N >> M >> V;
-    
+
     int tmp1, tmp2;
     for (int i = 0; i < M; i++) {
         cin >> tmp1 >> tmp2;
-        adjNodes[tmp1].emplace_back(tmp2);
-        adjNodes[tmp2].emplace_back(tmp1);
+        edges[tmp1].emplace_back(tmp2);
+        edges[tmp2].emplace_back(tmp1);
     }
 
-    for (int i = 1; i <= N; i++)
-        sort(adjNodes[i].begin(), adjNodes[i].end());
+    for (int i = 1; i < N + 1; i++)
+        sort(edges[i].begin(), edges[i].end());
 
     DFS(V);
 
-    memset(visited, false, sizeof(visited));
+    fill(visited, visited + N + 1, false);
     cout << endl;
 
     BFS(V);
