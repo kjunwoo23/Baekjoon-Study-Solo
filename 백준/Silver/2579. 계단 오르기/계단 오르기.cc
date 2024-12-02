2de91528e1253003966a3e2e9de83e63713da7ee
김준우 <kjunwoo23@naver.com>
2024-12-02 22:11:21 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,31 +1,36 @@
 #include <bits/stdc++.h>
+#define endl '\n'
+typedef long long ll;
 
 using namespace std;
 
-int N;
-int points[301];
-int memo[301][3];
-
-int main()
-{
-    cin.tie(NULL); cout.tie(NULL);
+void init() {
+    cin.tie(0);
+    cout.tie(0);
     ios_base::sync_with_stdio(false);
+}
+
+int N;
+int step[301];
+int dp[301][3];
+
+
+int main() {
+    init();
 
     cin >> N;
 
-    for (int i = 1; i < N + 1; i++)
-        cin >> points[i];
-
-    memo[1][1] = points[1];
-    memo[1][2] = points[1];
-
-    for (int i = 2; i <= N; i++) {
-        memo[i][0] = max(memo[i - 1][1], memo[i - 1][2]);
-        memo[i][1] = memo[i - 1][0] + points[i];
-        memo[i][2] = memo[i - 2][0] + points[i - 1] + points[i];
+    for (int i = 0; i < N; i++) {
+        cin >> step[i + 1];
     }
 
-    cout << max(memo[N][1], memo[N][2]);
+    for (int i = 1; i <= N; i++) {
+        dp[i][0] = max(dp[i - 1][1], dp[i - 1][2]);
+        dp[i][1] = dp[i - 1][0] + step[i];
+        dp[i][2] = dp[i - 1][1] + step[i];
+    }
+
+    cout << max(dp[N][1], dp[N][2]);
 
     return 0;
 }
