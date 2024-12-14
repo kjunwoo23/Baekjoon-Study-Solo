920fb95d9bba8822e82d02977a9c1179568fea4b
김준우 <kjunwoo23@naver.com>
2024-12-15 06:12:59 +0900

    [Gold IV] Title: 함께 블록 쌓기, Time: 4 ms, Memory: 2280 KB -BaekjoonHub

---
+++ 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+#define endl '\n'
+typedef long long ll;
+
+using namespace std;
+
+void init() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+}
+
+int N, M, H;
+vector<int> blocks[51];
+int dp[51][1001];
+
+int jaegui(int student, int curHeight) {
+    if (curHeight == H)   return 1;
+    if (student >= N + 1) return 0;
+
+    if (dp[student][curHeight] != -1)
+        return dp[student][curHeight];
+
+    int tmp = 0;
+    for (int i = 0; i < blocks[student].size(); i++) {
+        if (curHeight + blocks[student][i] <= H)
+            tmp = (tmp + jaegui(student + 1, curHeight + blocks[student][i]) % 10007) % 10007;
+    }
+    
+    dp[student][curHeight] = tmp;
+    return tmp;
+}
+
+int main() {
+    init();
+
+    cin >> N >> M >> H;
+    cin.ignore();
+
+    string str;
+    for (int i = 0; i < N; i++) {
+        getline(cin, str);
+
+        stringstream ss(str);
+        string token;
+
+        blocks[i].emplace_back(0);
+
+        while (getline(ss, token, ' '))
+            blocks[i].emplace_back(stoi(token));
+    }
+
+    memset(dp, -1, sizeof(dp));
+
+    cout << jaegui(0, 0) % 10007;
+
+    return 0;
+}
